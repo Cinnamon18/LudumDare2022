4cf9c1d5ad9f8ef146680768b03bcd41ce5602b4
Cinnamon18 <{11944881}+{Cinnamon18}@users.noreply.github.com>
2022-04-02 14:45:53 -0700

    setup tileset

--- GameObjects/MainCharacter.gd
+++ GameObjects/MainCharacter.gd
@@ -6,7 +6,6 @@ extends Node
 # var b = "text"
 var speed = Vector2(300, 300)
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,14 +14,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta):
+	var input = Vector2(0, 0)
 	if Input.is_action_pressed("ui_right"):
-		pass
+		input += Vector2.RIGHT
 	
 	if Input.is_action_pressed("ui_left"):
-		pass
+		input += Vector2.LEFT
 		
 	if Input.is_action_pressed("ui_up"):
-		pass
+		input += Vector2.UP
 	
 	if Input.is_action_pressed("ui_down"):
-		pass
+		input += Vector2.DOWN
+
+	self.position += input * delta * speed
