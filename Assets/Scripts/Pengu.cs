53e2d52db42fcdb5f2d553c957fa6fc690d43485
kusyu <kusyu.d@gmail.com>
2022-04-03 21:56:21 -0500

    sound effects aaa

--- Assets/Scripts/Pengu.cs
+++ Assets/Scripts/Pengu.cs
@@ -6,157 +6,188 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
-public class Pengu : MonoBehaviour {
+public class Pengu : MonoBehaviour
+{
 
-	[SerializeField] private Animator anim;
-	[SerializeField] private AudioManager audioManager;
-	[SerializeField] private TileManager tileManager;
-	[SerializeField] private Canvas popUp;
-	[SerializeField] private TMP_Text popupText;
+    [SerializeField] private Animator anim;
+    [SerializeField] private AudioManager audioManager;
+    [SerializeField] private TileManager tileManager;
+    [SerializeField] private Canvas popUp;
+    [SerializeField] private TMP_Text popupText;
 
-	[SerializeField] private Vector2 speed = new Vector2(3, 3);
+    [SerializeField] private Vector2 speed = new Vector2(3, 3);
 
-	[Header("My monos")]
-	[SerializeField] private Rigidbody2D rigidbody;
-	[SerializeField] private GameObject headIceBlock;
+    [Header("My monos")]
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private GameObject headIceBlock;
 
-	[Header("Tilemaps")]
-	[SerializeField] private Tilemap iceMap;
-	[SerializeField] private Tilemap borderMap;
-	[SerializeField] private Tilemap waterMap;
-	[SerializeField] private Tilemap collisionMap;
+    [Header("Tilemaps")]
+    [SerializeField] private Tilemap iceMap;
+    [SerializeField] private Tilemap borderMap;
+    [SerializeField] private Tilemap waterMap;
+    [SerializeField] private Tilemap collisionMap;
 
-	private PenguDir direction = PenguDir.IDLE_DOWN;
-	private bool isCaryingIce = false;
-	private float timeSurvived = 0;
+    private PenguDir direction = PenguDir.IDLE_DOWN;
+    private bool isCaryingIce = false;
+    private float timeSurvived = 0;
 
-	void Start() {
+    AudioSource squeak;
+    void Start()
+    {
+        squeak = this.GetComponent<AudioSource>();
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        this.rigidbody.velocity = speed * input;
 
-	// Update is called once per frame
-	void Update() {
-		var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		this.rigidbody.velocity = speed * input;
+        var rightPressed = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+        var leftPressed = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
+        var upPressed = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+        var downPressed = Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
+        var rightDown = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+        var leftDown = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        var upDown = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        var downDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
 
-		var rightPressed = Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
-		var leftPressed = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
-		var upPressed = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
-		var downPressed = Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
-		var rightDown = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
-		var leftDown = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
-		var upDown = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-		var downDown = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        if (rightPressed)
+        {
+            direction = PenguDir.RIGHT;
+            anim.SetTrigger("IsRight");
+            squeak.Play(0);
+        }
 
-		if (rightPressed) {
-			direction = PenguDir.RIGHT;
-			anim.SetTrigger("IsRight");
-		}
+        if (leftPressed)
+        {
+            direction = PenguDir.LEFT;
+            anim.SetTrigger("IsLeft");
+            squeak.Play(0);
+        }
 
-		if (leftPressed) {
-			direction = PenguDir.LEFT;
-			anim.SetTrigger("IsLeft");
-		}
+        if (upPressed)
+        {
+            direction = PenguDir.UP;
+            anim.SetTrigger("IsUp");
+            squeak.Play(0);
+        }
 
-		if (upPressed) {
-			direction = PenguDir.UP;
-			anim.SetTrigger("IsUp");
-		}
+        if (downPressed)
+        {
+            direction = PenguDir.DOWN;
+            anim.SetTrigger("IsDown");
+            squeak.Play(0);
+        }
 
-		if (downPressed) {
-			direction = PenguDir.DOWN;
-			anim.SetTrigger("IsDown");
-		}
+        if (!(rightDown || leftDown || upDown || downDown))
+        {
+            switch (direction)
+            {
+                case PenguDir.RIGHT:
+                    direction = PenguDir.IDLE_RIGHT;
+                    anim.SetTrigger("IsIdleRight");
+                    break;
+                case PenguDir.LEFT:
+                    direction = PenguDir.IDLE_LEFT;
+                    anim.SetTrigger("IsIdleLeft");
+                    break;
+                case PenguDir.UP:
+                    direction = PenguDir.IDLE_UP;
+                    anim.SetTrigger("IsIdleUp");
+                    break;
+                case PenguDir.DOWN:
+                    direction = PenguDir.IDLE_DOWN;
+                    anim.SetTrigger("IsIdleDown");
+                    break;
+            }
+        }
 
-		if (!(rightDown || leftDown || upDown || downDown)) {
-			switch (direction) {
-				case PenguDir.RIGHT:
-					direction = PenguDir.IDLE_RIGHT;
-					anim.SetTrigger("IsIdleRight");
-					break;
-				case PenguDir.LEFT:
-					direction = PenguDir.IDLE_LEFT;
-					anim.SetTrigger("IsIdleLeft");
-					break;
-				case PenguDir.UP:
-					direction = PenguDir.IDLE_UP;
-					anim.SetTrigger("IsIdleUp");
-					break;
-				case PenguDir.DOWN:
-					direction = PenguDir.IDLE_DOWN;
-					anim.SetTrigger("IsIdleDown");
-					break;
-			}
-		}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryHoistOrPlaceIceBlock();
+        }
 
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			TryHoistOrPlaceIceBlock();
-		}
+        timeSurvived += Time.deltaTime;
+    }
 
-		timeSurvived += Time.deltaTime;
-	}
+    public void KillIfOnWater(Vector3Int tilePos)
+    {
+        if (iceMap.WorldToCell(transform.position) == tilePos && popUp.gameObject.activeSelf == false)
+        {
+            anim.SetTrigger("IsPerish");
+            // Game over
+            Debug.Log("You lose :(");
+            // Time.timeScale = 0;
+            popupText.text = popupText.text + " " + string.Format("{0:F1}", timeSurvived) + "s";
+            popUp.gameObject.SetActive(true);
+        }
+    }
 
-	public void KillIfOnWater(Vector3Int tilePos) {
-		if (iceMap.WorldToCell(transform.position) == tilePos && popUp.gameObject.activeSelf == false) {
-			anim.SetTrigger("IsPerish");
-			// Game over
-			Debug.Log("You lose :(");
-			// Time.timeScale = 0;
-			popupText.text = popupText.text + " " +string.Format("{0:F1}", timeSurvived) + "s";
-			popUp.gameObject.SetActive(true);
-		}
-	}
+    private void TryHoistOrPlaceIceBlock()
+    {
+        if (isCaryingIce)
+        {
+            // audioManager.PlaySound(Sound.INVALID_ACTION);
+            TryPlaceIceBlock();
+        }
+        else
+        {
+            TryHoistIceBlock();
+        }
+    }
 
-	private void TryHoistOrPlaceIceBlock() {
-		if (isCaryingIce) {
-			// audioManager.PlaySound(Sound.INVALID_ACTION);
-			TryPlaceIceBlock();
-		} else {
-			TryHoistIceBlock();
-		}
-	}
+    private void TryHoistIceBlock()
+    {
+        var tilesInFront = GetTilesInFront();
+        if (tilesInFront[0] != null)
+        {
+            isCaryingIce = true;
+            headIceBlock.SetActive(true);
 
-	private void TryHoistIceBlock() {
-		var tilesInFront = GetTilesInFront();
-		if (tilesInFront[0] != null) {
-			isCaryingIce = true;
-			headIceBlock.SetActive(true);
+            tileManager.SetTileNonIcey(GetTileInFrontPos());
+        }
+        else
+        {
+            audioManager.PlaySound(Sound.INVALID_ACTION);
+        }
+    }
 
-			tileManager.SetTileNonIcey(GetTileInFrontPos());
-		} else {
-			audioManager.PlaySound(Sound.INVALID_ACTION);
-		}
-	}
+    private void TryPlaceIceBlock()
+    {
+        var tilesInFront = GetTilesInFront();
+        if (tilesInFront[0] == null)
+        {
+            isCaryingIce = false;
+            headIceBlock.SetActive(false);
 
-	private void TryPlaceIceBlock() {
-		var tilesInFront = GetTilesInFront();
-		if (tilesInFront[0] == null) {
-			isCaryingIce = false;
-			headIceBlock.SetActive(false);
+            tileManager.SetTileIcey(GetTileInFrontPos());
+        }
+        else
+        {
+            audioManager.PlaySound(Sound.INVALID_ACTION);
+        }
+    }
 
-			tileManager.SetTileIcey(GetTileInFrontPos());
-		} else {
-			audioManager.PlaySound(Sound.INVALID_ACTION);
-		}
-	}
+    private Vector3Int GetTileInFrontPos()
+    {
+        Vector3Int tileInFront = iceMap.WorldToCell(transform.position);
+        if (direction == PenguDir.LEFT || direction == PenguDir.IDLE_LEFT) { tileInFront += Vector3Int.left; }
+        if (direction == PenguDir.RIGHT || direction == PenguDir.IDLE_RIGHT) { tileInFront += Vector3Int.right; }
+        if (direction == PenguDir.UP || direction == PenguDir.IDLE_UP) { tileInFront += Vector3Int.up; }
+        if (direction == PenguDir.DOWN || direction == PenguDir.IDLE_DOWN) { tileInFront += Vector3Int.down; }
 
-	private Vector3Int GetTileInFrontPos() {
-		Vector3Int tileInFront = iceMap.WorldToCell(transform.position);
-		if (direction == PenguDir.LEFT || direction == PenguDir.IDLE_LEFT) { tileInFront += Vector3Int.left; }
-		if (direction == PenguDir.RIGHT || direction == PenguDir.IDLE_RIGHT) { tileInFront += Vector3Int.right; }
-		if (direction == PenguDir.UP || direction == PenguDir.IDLE_UP) { tileInFront += Vector3Int.up; }
-		if (direction == PenguDir.DOWN || direction == PenguDir.IDLE_DOWN) { tileInFront += Vector3Int.down; }
+        return tileInFront;
+    }
 
-		return tileInFront;
-	}
-
-	private List<TileBase> GetTilesInFront() {
-		var tileInFrontPos = GetTileInFrontPos();
-		return new List<TileBase>() {
-			iceMap.GetTile<TileBase>(tileInFrontPos),
-			borderMap.GetTile<TileBase>(tileInFrontPos),
-			waterMap.GetTile<TileBase>(tileInFrontPos),
-			collisionMap.GetTile<TileBase>(tileInFrontPos)
-		};
-	}
+    private List<TileBase> GetTilesInFront()
+    {
+        var tileInFrontPos = GetTileInFrontPos();
+        return new List<TileBase>() {
+            iceMap.GetTile<TileBase>(tileInFrontPos),
+            borderMap.GetTile<TileBase>(tileInFrontPos),
+            waterMap.GetTile<TileBase>(tileInFrontPos),
+            collisionMap.GetTile<TileBase>(tileInFrontPos)
+        };
+    }
 }
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -3,90 +3,106 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class TileManager : MonoBehaviour {
+public class TileManager : MonoBehaviour
+{
 
-	[SerializeField] private float timeBetweenTileCracks = 5f;
-	[SerializeField] private float percentDecreaseAfterCracks = 0.95f;
+    [SerializeField] private float timeBetweenTileCracks = 5f;
+    [SerializeField] private float percentDecreaseAfterCracks = 0.95f;
 
-	[SerializeField] private Pengu pengu;
+    [SerializeField] private Pengu pengu;
 
-	[Header("Melting")]
-	[SerializeField] private float timeForTileToCrackPerFrame = 0.2f;
-	[SerializeField] private List<TileBase> meltTiles;
+    [Header("Melting")]
+    [SerializeField] private float timeForTileToCrackPerFrame = 0.2f;
+    [SerializeField] private List<TileBase> meltTiles;
 
-	[Header("Tilemaps")]
-	[SerializeField] private Tilemap iceMap;
-	[SerializeField] private Tilemap borderMap;
-	[SerializeField] private Tilemap waterMap;
-	[SerializeField] private Tilemap collisionMap;
+    [Header("Tilemaps")]
+    [SerializeField] private Tilemap iceMap;
+    [SerializeField] private Tilemap borderMap;
+    [SerializeField] private Tilemap waterMap;
+    [SerializeField] private Tilemap collisionMap;
 
-	[Header("Tiles")]
-	[SerializeField] private TileBase iceTile;
-	[SerializeField] private TileBase shallowBorderTile;
+    [Header("Tiles")]
+    [SerializeField] private TileBase iceTile;
+    [SerializeField] private TileBase shallowBorderTile;
 
-	private float meltTimer = 0;
+    private float meltTimer = 0;
+    AudioSource ac;
 
-	void Start() {
-		meltTimer = timeBetweenTileCracks;
-	}
+    void Start()
+    {
+        meltTimer = timeBetweenTileCracks;
+        ac = this.GetComponent<AudioSource>();
+    }
 
-	void Update() {
-		meltTimer -= Time.deltaTime;
+    void Update()
+    {
+        meltTimer -= Time.deltaTime;
 
-		if (meltTimer < 0) {
-			timeBetweenTileCracks *= percentDecreaseAfterCracks;
-			meltTimer = timeBetweenTileCracks;
+        if (meltTimer < 0)
+        {
+            timeBetweenTileCracks *= percentDecreaseAfterCracks;
+            meltTimer = timeBetweenTileCracks;
 
-			StartCoroutine(MeltTile(GetRandomIceTile()));
-		}
-	}
+            StartCoroutine(MeltTile(GetRandomIceTile()));
+        }
+    }
 
-	public IEnumerator MeltTile(Vector3Int tilePos) {
-		SetTileNonIcey(tilePos);
-		// Let player walk on the crumbling tile until its fully crumbled!
-		collisionMap.SetTile(tilePos, null);
+    public IEnumerator MeltTile(Vector3Int tilePos)
+    {
+        SetTileNonIcey(tilePos);
+        // Let player walk on the crumbling tile until its fully crumbled!
+        collisionMap.SetTile(tilePos, null);
 
-		foreach (var meltTile in meltTiles) {
-			iceMap.SetTile(tilePos, meltTile);
-			yield return new WaitForSeconds(timeForTileToCrackPerFrame);
-		}
-		SetTileNonIcey(tilePos);
+        foreach (var meltTile in meltTiles)
+        {
+            ac.Play(0);
+            iceMap.SetTile(tilePos, meltTile);
+            yield return new WaitForSeconds(timeForTileToCrackPerFrame);
 
-		pengu.KillIfOnWater(tilePos);
-	}
+        }
+        SetTileNonIcey(tilePos);
 
-	public Vector3Int GetRandomIceTile() {
-		var localTilePoses = new List<Vector3Int>();
+        pengu.KillIfOnWater(tilePos);
+    }
 
-		foreach (var pos in iceMap.cellBounds.allPositionsWithin) {
-			Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
-			if (iceMap.HasTile(localPlace)) {
-				localTilePoses.Add(localPlace);
-			}
-		}
+    public Vector3Int GetRandomIceTile()
+    {
+        var localTilePoses = new List<Vector3Int>();
 
-		return localTilePoses[Random.Range(0, localTilePoses.Count)];
-	}
+        foreach (var pos in iceMap.cellBounds.allPositionsWithin)
+        {
+            Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+            if (iceMap.HasTile(localPlace))
+            {
+                localTilePoses.Add(localPlace);
+            }
+        }
 
-	public void SetTileIcey(Vector3Int tileCoords) {
-		iceMap.SetTile(tileCoords, iceTile);
-		borderMap.SetTile(tileCoords, null);
-		borderMap.SetTile(tileCoords + Vector3Int.down, shallowBorderTile);
-		collisionMap.SetTile(tileCoords, null);
+        return localTilePoses[Random.Range(0, localTilePoses.Count)];
+    }
 
-		iceMap.RefreshTile(tileCoords);
-		borderMap.RefreshTile(tileCoords);
-	}
+    public void SetTileIcey(Vector3Int tileCoords)
+    {
+        iceMap.SetTile(tileCoords, iceTile);
+        borderMap.SetTile(tileCoords, null);
+        borderMap.SetTile(tileCoords + Vector3Int.down, shallowBorderTile);
+        collisionMap.SetTile(tileCoords, null);
 
-	public void SetTileNonIcey(Vector3Int tileCoords) {
-		iceMap.SetTile(tileCoords, null);
-		borderMap.SetTile(tileCoords + Vector3Int.down, null);
-		if (iceMap.GetTile(tileCoords + Vector3Int.up) != null) {
-			borderMap.SetTile(tileCoords, shallowBorderTile);
-		}
-		collisionMap.SetTile(tileCoords, iceTile);
+        iceMap.RefreshTile(tileCoords);
+        borderMap.RefreshTile(tileCoords);
+    }
 
-		iceMap.RefreshTile(tileCoords);
-		borderMap.RefreshTile(tileCoords);
-	}
+    public void SetTileNonIcey(Vector3Int tileCoords)
+    {
+        iceMap.SetTile(tileCoords, null);
+        borderMap.SetTile(tileCoords + Vector3Int.down, null);
+        if (iceMap.GetTile(tileCoords + Vector3Int.up) != null)
+        {
+            borderMap.SetTile(tileCoords, shallowBorderTile);
+        }
+        collisionMap.SetTile(tileCoords, iceTile);
+
+        iceMap.RefreshTile(tileCoords);
+        borderMap.RefreshTile(tileCoords);
+    }
 }
